1504d4009f4170a65d2d1e5f2f7d03ba6f1385aa
bahrulrozak <bahrulrozak02@gmail.com>
2025-03-13 15:44:55 +0700

    feat (master employees) : create

--- app/Http/Controllers/EmployeesController.php
+++ app/Http/Controllers/EmployeesController.php
@@ -9,4 +9,8 @@ class EmployeesController extends Controller
     public function index(){
         return view('admin.backend.employees.index');
     }
+
+    public function create(){
+        return view('admin.backend.employees.create');
+    }
 }
